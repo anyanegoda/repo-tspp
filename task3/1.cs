02ddab1f7542c936390b821bb3813eda41d9a34d
anyane <negoda.anya@gmail.com>
2016-12-24 21:06:42 +0300

    подъём и спуск метода после

--- task3/1.cs
+++ task3/1.cs
@@ -1,15 +1,13 @@
 class Employee {
-boolean isPriviliged() { }
-void assignCar() { }
+void initialize() {
+if (isPriviliged())
+assignCar();
+}
 }
 class Manager {
 public Manager (String name, String id, int grade) {
-super (name, id);
+super (name, id):
 _grade = grade;
-if (isPriviliged())
-assignCar(); //это выполн€ют все подклассы
-}
-boolean isPriviliged(){
-return _grade > 4;
+Initialize();
 }
 }

7b91dcf50581cf78c97c06f39952d3a71e96c859
anyane <negoda.anya@gmail.com>
2016-12-24 20:57:29 +0300

    удаление управляющего флага после

--- task2/4.cs
+++ task2/4.cs
@@ -1,18 +1,14 @@
 void CheckSecurity(string[] people)
 {
-  bool found = false;
   for (int i = 0; i < people.Length; i++)
   {
-    if (!found)
-    {
-      if (people[i].Equals("Don")) {
-        SendAlert();
-        found = true;
-      }
-      if (people[i].Equals("John")) {
-        SendAlert();
-        found = true;
-      }
+    if (people[i].Equals("Don")) {
+      SendAlert();
+      break;
+    }
+    if (people[i].Equals("John")) {
+      SendAlert();
+      break;
     }
   }
 }

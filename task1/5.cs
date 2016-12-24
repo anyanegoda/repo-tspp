00e605db89cfdfea64c92cb5ae4a37ec88cf8b79
anyane <negoda.anya@gmail.com>
2016-12-24 20:30:57 +0300

    замена переменной после

--- task1/5.cs
+++ task1/5.cs
@@ -1,13 +1,15 @@
 double CalculateTotal() 
 {
-  double basePrice = quantity * itemPrice;
-  
-  if (basePrice > 1000) 
+  if (BasePrice() > 1000) 
   {
-    return basePrice * 0.95;
+    return BasePrice() * 0.95;
   }
   else 
   {
-    return basePrice * 0.98;
+    return BasePrice() * 0.98;
   }
+}
+double BasePrice() 
+{
+  return quantity * itemPrice;
 }
